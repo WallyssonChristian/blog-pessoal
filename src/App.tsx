b191c131779e3d98e051f55b132221006bc4bc0b
Wallysson Araujo <Cerberus74@live.com>
2024-01-17 21:33:51 -0300

    Tailwind implement

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import Home from './paginas/home/Home.tsx';
+import Navbar from './components/navbar/Navbar.tsx';
+import Footer from './components/footer/Footer.tsx';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-   <Home />
+    <>
+      <BrowserRouter>
+        <Navbar />
+
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+
+        <Footer />
+      </BrowserRouter>
+    </>
+
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,27 +1,32 @@
-import './Home.css';
-
 function Home() {
     return (
         <>
-            <div style={{
-                width: "100vw",
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <div style={{
-                    width: "80vw",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center"
-                }}>
-                    <div style={{
-                        width: "80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center"
-                    }}>
-                        <h1 >Home</h1>
-                        <img src='https://i.imgur.com/VpwApCU.png' alt="Imagem da página home" width="400px"></img>
+            <div id="container" className='
+            flex justify-center bg-indigo-900'>
+
+                <div id="subcontainer" className='
+                container grid grid-cols-2 text-white'>
+
+                    <div id="texto" className='
+                    flex flex-col gap-4 items-center justify-center py-4'>
+
+                        <h2 className="
+                        text-5x1 font-bold">Seja Bem Vindo!</h2>
+                        <p className="text-x1">Lugar para desabafar!</p>
+
+                        <div className="
+                        flex justify-around gap-4">
+                            <div className="
+                            rounded text-white border-white border-solid border-2 px-4 py-2">
+                                Postagem
+                            </div>
+                        </div>
+
+                        <div id="imagem" className="
+                        flex justify-center">
+                            <img src='https://i.imgur.com/VpwApCU.png' alt="Imagem da página home" className="w-2/3"></img>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
